798ba7c4b9cf30f017e4f36c5cf69fd0eca33e7f
Hikkun098 <mhhkn098@gmail.com>
2020-10-14 18:34:33 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  validates :nickname, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,format: { with: VALID_EMAIL_REGEX }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :birthday, presence: true
+
+  has_many :books
+  has_many :comments
 end
