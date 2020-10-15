eec73070613620a06ef2a2c1ddfb4b6f1c0cc67d
Hikkun098 <mhhkn098@gmail.com>
2020-10-15 23:28:33 +0900

    コントローラーの設定

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,13 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.includes(:user).order("created_at DESC")
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+end
