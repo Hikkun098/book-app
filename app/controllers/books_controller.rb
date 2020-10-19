66bbe8697b2754676c4683cd0892f7dce5759b01
Hikkun098 <mhhkn098@gmail.com>
2020-10-19 15:11:49 +0900

    コントローラーの設定2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,6 +33,7 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
     @book.update(book_params)
     if @book.save
       redirect_to root_path
