c3032cca4cf62d858cdc782a871f1bde2201bfa6
Hikkun098 <mhhkn098@gmail.com>
2020-10-19 11:54:46 +0900

    Active Storageの導入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,5 +8,16 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:image, :book_name, :description, :category_id, :age, :price).merge(user_id: current_user.id)
   end
 end
